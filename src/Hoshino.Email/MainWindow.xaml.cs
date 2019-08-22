d716ae1b792d8dfd555d263649b8ee183c2a0224
river.xu <1842125096@qq.com>
2019-08-22 17:40:30 +0800

    范德薩

--- src/Hoshino.Email/MainWindow.xaml.cs
+++ src/Hoshino.Email/MainWindow.xaml.cs
@@ -1,5 +1,4 @@
-﻿using MahApps.Metro.Controls;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -18,16 +17,29 @@ namespace Hoshino.Email
     /// <summary>
     /// MainWindow.xaml 的交互逻辑
     /// </summary>
-    public partial class MainWindow : MetroWindow
+    public partial class MainWindow : Window
     {
+        public class Menu
+        {
+            public string Name { set; get; }
+        }
         public MainWindow()
         {
             InitializeComponent();
+            List<Menu> MenuList = new List<Menu>();
+            MenuList.Add(new Menu { Name = "首頁" });
+            MenuList.Add(new Menu { Name = "通訊錄" });
+            MenuList.Add(new Menu { Name = "郵件管理" });
+            MenuList.Add(new Menu { Name = "新增郵件" });
+            MenuList.Add(new Menu { Name = "郵件篩選" });
+
+            ItemsListBox.ItemsSource = MenuList;
         }
 
-        #region 控件的事件
-        private void BtnMain_Click(object sender, RoutedEventArgs e)
+        #region 控件事件
+        private void ItemsListBox_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
         {
+            ListBoxItem lbi = ((sender as ListBox).SelectedItem as ListBoxItem);
 
         }
         #endregion
