432ce28c2b6a9346f146a0a451d26c0ad587c84f
river.xu <1842125096@qq.com>
2019-10-11 20:32:57 +0800

    導出excel邏輯

---
+++ src/Hoshino.Email/Controls/EmailInfoManage/Win_ExportExcel.xaml.cs
@@ -0,0 +1,72 @@
+﻿using Hoshino.Email.Controls.Common;
+using Hoshino.Email.Core;
+using Hoshino.Email.Core.Helper;
+using Hoshino.Email.Entity;
+using Hoshino.Email.Helper;
+using Hoshino.Email.Repository;
+using NPOI.HSSF.UserModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Hoshino.Email.Controls.EmailInfoManage
+{
+    /// <summary>
+    /// Win_NewMoreEmail.xaml 的交互逻辑
+    /// </summary>
+    public partial class Win_ExportExcel : Window
+    {
+        EmailSendBccAccountRepository ESBR_Repository = new EmailSendBccAccountRepository();
+
+        public int EmailID;
+        public Win_ExportExcel(int EmailID)
+        {
+            InitializeComponent();
+            this.EmailID = EmailID;
+        }
+
+        private void BtnSuccess_Click(object sender, RoutedEventArgs e)
+        {
+            ExportExcel(1);
+        }
+
+        private void BtnFail_Click(object sender, RoutedEventArgs e)
+        {
+            ExportExcel(-1);
+        }
+
+        private void BtnStop_Click(object sender, RoutedEventArgs e)
+        {
+            ExportExcel(2);
+        }
+
+        private void BtnNoStart_Click(object sender, RoutedEventArgs e)
+        {
+            ExportExcel(0);
+        }
+
+        public void ExportExcel(int state)
+        {
+            List<EmailBccAccountEntity> bccAccountList = ESBR_Repository.GetExportEmailBccAccountList(EmailID, state).ToList();
+            if (bccAccountList == null || bccAccountList.Count <= 0)
+            {
+                "不可以導出空數據".ShowDialog();
+                return;
+            }
+            ExportBccAccountHelper helper = new ExportBccAccountHelper();
+            helper.Export(bccAccountList);
+        }
+
+    }
+}
---
+++ src/Hoshino.Email/Helper/ExportBccAccountHelper.cs
@@ -0,0 +1,64 @@
+﻿using Hoshino.Email.Entity;
+using NPOI.HSSF.UserModel;
+using NPOI.SS.UserModel;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hoshino.Email.Helper
+{
+    public class ExportBccAccountHelper
+    {
+        public void Export(List<EmailBccAccountEntity> _EmailBccAccountEntity)
+        {
+
+            SaveFileDialog sfd = new SaveFileDialog();
+            sfd.Filter = "Excel文件(*.xls)|*.xls|Excel文件(*.xlsx)|*.xlsx|所有文件(*.*)|*.*";
+            if (sfd.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    Stream _OpenFileStream = sfd.OpenFile();
+                    CreateSheet(_EmailBccAccountEntity, _OpenFileStream);
+                    "數據導出成功！".ShowDialog();
+                }
+                catch (Exception ex)
+                {
+                    //LogHelper.Error("數據導出", "數據導出", ex.Message, ex);
+                    //MessageBox.Show("數據導出失敗！");
+                }
+            }
+        }
+        void CreateSheet(List<EmailBccAccountEntity> _EmailBccAccountEntity, Stream _OpenFileStream)
+        {
+            IWorkbook workbook = new HSSFWorkbook();//创建Workbook对象
+            ISheet sheet = workbook.CreateSheet("Sheet1");//创建工作表
+            IRow row = sheet.CreateRow(0);//在工作表中添加一行
+            ICell cellName = row.CreateCell(0);//在行中添加一列
+            cellName.SetCellValue("分類");//设置列的内容
+            ICell cellEmail = row.CreateCell(1);//在行中添加一列
+            cellEmail.SetCellValue("郵箱地址");//设置列的内容
+            int i = 1;
+            foreach (var one in _EmailBccAccountEntity)
+            {
+                IRow newRow = sheet.CreateRow(i);//在工作表中添加一行
+                ICell newCellName = newRow.CreateCell(0);//在行中添加一列
+                newCellName.SetCellValue(one.EmailBccAccountCategoryName);//设置列的内容
+                ICell newCellEmail = newRow.CreateCell(1);//在行中添加一列
+                newCellEmail.SetCellValue(one.EmailBccAccountAddress);//设置列的内容
+                i++;
+            }
+            if (_OpenFileStream == null)
+            {
+                return;
+            }
+            workbook.Write(_OpenFileStream);
+            _OpenFileStream.Close();
+            _OpenFileStream.Dispose();
+        }
+    }
+}
