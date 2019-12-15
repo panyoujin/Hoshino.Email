5c042f9b46ac71fee12bf4fea4cdc42280bdea4e
River <1842125096@qq.com>
2019-12-15 22:41:22 +0800

    发件箱管理增加导入删除邮件功能

--- src/Hoshino.Email/Controls/SendEmailManage/UC_SendEmail.xaml.cs
+++ src/Hoshino.Email/Controls/SendEmailManage/UC_SendEmail.xaml.cs
@@ -1,7 +1,12 @@
 ﻿using Hoshino.Email.Controls.Common;
+using Hoshino.Email.Core;
+using Hoshino.Email.Core.Helper;
 using Hoshino.Email.Entity;
 using Hoshino.Email.Repository;
+using NPOI.HSSF.UserModel;
 using System;
+using System.Collections.Generic;
+using System.IO;
 using System.Windows;
 using System.Windows.Controls;
 
@@ -50,7 +55,7 @@ namespace Hoshino.Email.Controls.SendEmailManage
 
         private void BtnNewMoreEmail_Click(object sender, RoutedEventArgs e)
         {
-            bool? result= new Win_NewMoreEmail().ShowDialog();
+            bool? result = new Win_NewMoreEmail().ShowDialog();
             if (result.HasValue && result.Value)
             {
                 GetList();
@@ -82,6 +87,92 @@ namespace Hoshino.Email.Controls.SendEmailManage
                 EA_Repository.Delete(entity.EmailAccountID);
                 this.ucPage.RefreshList(false);
             });
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnDeleteExport_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                System.Windows.Forms.OpenFileDialog openFile = new System.Windows.Forms.OpenFileDialog();
+                openFile.Filter = "Excel文件(*.xls)|*.xls|Excel文件(*.xlsx)|*.xlsx|所有文件(*.*)|*.*";
+                if (openFile.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    List<string> failCount = DeleteEmailData(openFile.FileName);
+
+                    if (failCount != null && failCount.Count > 0)
+                    {
+                        int count = failCount.Count;
+                        string failStr = "";
+                        int i = 0;
+                        foreach (var str in failCount)
+                        {
+                            i++;
+                            failStr += str;
+                            if (i != count)
+                            {
+                                failStr += " | ";
+                            }
+                        }
+                        string.Format("刪除失敗郵箱：{0},其他刪除成功", failStr).ShowDialog();
+                    }
+                    else
+                    {
+                        "郵箱刪除成功".ShowDialog();
+                    }
+                    this.ucPage.RefreshList(false);
+
+                }
+            }
+            catch (Exception ex)
+            {
+                ex.Message.ShowDialog();
+            }
+        }
+
+        private List<string> DeleteEmailData(string filePath)
+        {
+            List<string> failAccount = new List<string>();
+            List<FailEmailAccountEntity> failEntity = new List<FailEmailAccountEntity>();
+
+            FileStream stream = File.Open(filePath, FileMode.Open, FileAccess.Read);
+            //根据路径通过已存在的excel来创建HSSFWorkbook，即整个excel文档
+            HSSFWorkbook workbook = new HSSFWorkbook(stream);
+            //获取excel的第一个sheet
+            HSSFSheet sheet = (HSSFSheet)workbook.GetSheetAt(0);
+            //最后一列的标号  即总的行数
+            int rowCount = sheet.LastRowNum;
+            int total = 0;
+            for (int i = (sheet.FirstRowNum + 1); i < sheet.LastRowNum + 1; i++)
+            {
+                total++;
+                FailEmailAccountEntity ea = new FailEmailAccountEntity();
+                try
+                {
+                    HSSFRow row = (HSSFRow)sheet.GetRow(i);
+                    if (row.GetCell(0) != null)
+                        ea.EmailAccountName = row.GetCell(0).ToString();
+                    if (row.GetCell(1) != null)
+                        ea.EmailAccountAddress = row.GetCell(1).ToString();
+
+
+                    if (!EA_Repository.Delete(ea.EmailAccountAddress))
+                    {
+                        failAccount.Add(ea.EmailAccountAddress);
+                    }
+                }
+                catch (Exception ex)
+                {
+                    LogHelper.Error(string.Format("删除郵箱【{0}】異常", ea.EmailAccountAddress), ex);
+                    ea.FailMessage = ex.Message;
+                    failEntity.Add(ea);
+                }
+            }
+            return failAccount;
         }
     }
 }
